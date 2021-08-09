fea21d2da6d6f5911cc54c3a1b19e8817f8f2b29
Iliasse <elabouyi.iliasse@gmail.com>
2021-08-10 00:27:00 +0200

    factorization

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.querySelector(".modal-body > form");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,14 +21,12 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-
-// add red warning message if user input is invalid (prend 2 paramètres: nom de l'id de l'input / index de l'input qui renvoit un message d'erreur)
+// add red warning content if user input is invalid (prend 2 paramètres: nom de l'id de l'input / index de l'input qui renvoit un content d'erreur)
 function invalidWarning(target, index) {
 
   const errorMessage = [
@@ -42,73 +41,84 @@ function invalidWarning(target, index) {
 
   if (!target.validity.valid) {
 
-    const node = document.createElement("P");
-    const textnode = document.createTextNode(errorMessage[index]);
-    node.appendChild(textnode);
-    target.parentNode.after(node);
-    node.className = "data-error";
-    target.classList.add("input-error");
+  submissionSuccedMessage("p", errorMessage[index], "data-error", target)
+
   }
 }
 
-// gather all inputs in an array
+// gather all form inputs in an array
 let inputsArray = Array.prototype.slice.call(document.querySelectorAll(".text-control"));
 inputsArray.push(document.querySelector(".checkbox-input"));
 inputsArray.push(document.getElementById("checkbox1"));
 
 
+// add new html element to modal when submission succeded
+function submissionSuccedMessage(htmlElement, content, className, position) {
+
+  const node = document.createElement(""+htmlElement);
+  const textnode = document.createTextNode(""+content);
+  node.appendChild(textnode);
+
+  if (position == form) {
+    position.after(node);
+    node.className = ""+className;
+  }
+
+  else {
+    position.parentNode.after(node);
+    node.className = ""+className;
+    position.classList.add("input-error");
+  }
+  
+}
+
+function inputsChecker() {
+
+  let invalidInputs = inputsArray.length;
+  for (let i = 0; i < inputsArray.length; i++) {
+    if (inputsArray[i].validity.valid) {
+      invalidInputs --;
+    }
+  }
+  return invalidInputs
+}
+
+// removes red error messages
+function errorMessageRemover(className) {
+  let target = document.querySelectorAll(""+className);
+  if (className == ".input-error") {
+    for (let i=0; i < target.length; i++) {
+      target[i].classList.remove("input-error");
+    }
+  }
+  else {
+    for (let i=0; i < target.length; i++) {
+      target[i].remove();
+    }
+  }
+}
 
 // submit form
 function validate() {
 
-  let validaity = inputsArray.length;
-  console.log(validaity)
-
-  for (let i = 0; i < inputsArray.length; i++) {
-    if (inputsArray[i].validity.valid) {
-      validaity --;
-      console.log(validaity);
-    }
-  }
-
   // Removes all error messages
-  let errorMessages = document.querySelectorAll(".data-error");
-  for (let i=0; i < errorMessages.length; i++) {
-    errorMessages[i].remove();
-  }
-
+  errorMessageRemover(".data-error");
+  
   // removes all input red border
-  let errorInputs = document.querySelectorAll(".input-error");
-  for (let i=0; i < errorInputs.length; i++) {
-    errorInputs[i].classList.remove("input-error");
-  }
+  errorMessageRemover(".input-error");
 
+  if (inputsChecker() < 1) {
 
-  if (validaity < 1) {
-
-    // message of submission
-    const form = document.querySelector(".modal-body > form");
-
-    const node = document.createElement("P");
-    const textnode = document.createTextNode("Merci ! Votre réservation a été reçue.");
-    node.appendChild(textnode);
-    form.parentNode.after(node);
-    node.className = "form-validation-message";
-
+    // delete data and hide form
+    document.getElementById("form").reset();
     form.style.display = "none";
 
-    // add ok button
-    const node2 = document.createElement("button");
-    const textnode2 = document.createTextNode("Ok");
-    node2.appendChild(textnode2);
-    node.after(node2);
-    node2.className = "btn-submit btn-ok";
+    // add ok button & content of sumbission
+    submissionSuccedMessage("button", "Ok", "btn-submit btn-ok", form)
+    submissionSuccedMessage("p", "Merci ! Votre réservation a été reçue.", "form-validation-content", form)
 
-    // add closing func to "Ok" button
-    node2.setAttribute("onclick", "closeModal();")
-
-    // delete data
-    document.getElementById("form").reset();
+    // add up closing functionality to "Ok" button
+    document.querySelector(".btn-ok").setAttribute("onclick", "closeModal();")
   }
 
   else {
